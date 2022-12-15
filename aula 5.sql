51dee5a86b471288c760173a4a4d608c7b667d28
André Kaique <112278911+AndrekaiqueDevjunior@users.noreply.github.com>
2022-12-15 08:25:16 -0300

    Add files via upload

---
+++ aula 5.sql
@@ -0,0 +1,12 @@
+
+
+insert  into pessoas
+( id, nome, nascimento , sexo, peso, altura, nacionalidade)
+values
+( default ,'Castiel', '2023-01-14','M',' 64', '1.75', default),
+( default ,'Ana Maria', '2023-01-14','F','55', '1.75', default),
+( default ,'Lucinete', '2023-01-14','F','67', '1.75', default),
+( default ,'Pedro', '2023-01-14','M','85.10', '1.75', default),
+( default ,'Matheus', '2023-01-14','M','100.32', '1.75', default);
+
+select * from pessoas;
